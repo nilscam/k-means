1e4fd27979c45775954aadb9fe18906d8c32125b
Nils Cambreleng <nilscambreleng@gmail.com>
2018-05-27 17:36:36 +0200

    push working algo perfect

--- hpack/app/Main.hs
+++ hpack/app/Main.hs
@@ -56,33 +56,38 @@ main = do
 
 compress :: [Pixel] -> Int -> Float -> IO ()
 compress listPixels nbCentroid convergence = do
-
   let gen1 = firstGeneration nbCentroid listPixels
-  print gen1
   kmean listPixels gen1 nbCentroid convergence
-
 
 kmean :: [Pixel] -> Generation -> Int -> Float -> IO ()
 kmean listPixels prevgen nbCentroid convergence = do
 
   -- recompute pixels associations with centroids
   let newgen = makeNewGen prevgen listPixels
-
-  print newgen
-
-  {-
-  -- recompute centroids positions
   let newGenUpdate = computeCentroids newgen nbCentroid
+  let maxMoveDistance = maxCentroidsMoveDistance prevgen newGenUpdate
 
-  let maxMoveDistance = maxCentroidsMoveDistance prevgen newgen
+  print maxMoveDistance
 
   if (maxMoveDistance <= convergence)
-    then printCompressedImage newgen
-    else kmean listPixels newgen nbCentroid convergence
--}
+    --then printCompressedImage newgen
+    then putStrLn "Success"
+    else kmean listPixels newGenUpdate nbCentroid convergence
 
 dupCentroidList :: Generation -> Generation
 dupCentroidList origin = Generation [(Cluster (centroid x) []) | x <- (clusters origin)]
+
+-- compare 2 generation and return biggest cluster move
+minCluster :: [Cluster] -> [Cluster] -> Float
+minCluster [] [] = 99999
+minCluster [x] [y] = calcDistC (centroid x) (centroid y)
+minCluster (x:xs) (y:ys) = min (calcDistC (centroid x) (centroid y)) (minCluster xs ys)
+
+maxCentroidsMoveDistance :: Generation -> Generation -> Float
+maxCentroidsMoveDistance old new = minCluster (clusters old) (clusters new)
+
+calcDistC :: Centroid -> Centroid -> Float
+calcDistC c1 c2 = sqrt(((px (cpos c1)) - (px (cpos c2))) ^ 2 + ((py (cpos c1)) - (py (cpos c2))) ^ 2 + ((pz (cpos c1)) - (pz (cpos c2))) ^ 2)
 
 calcDistance :: Pixel -> Centroid -> Float
 calcDistance pixel centroid = sqrt(((px (color pixel)) - (px (cpos centroid))) ^ 2 + ((py (color pixel)) - (py (cpos centroid))) ^ 2 + ((pz (color pixel)) - (pz (cpos centroid))) ^ 2)
@@ -100,6 +105,17 @@ getClosest pixel (x:xs)
 makeNewGen :: Generation -> [Pixel] -> Generation
 makeNewGen oldgen listPixels = Generation (map (\x -> Cluster (centroid x) [ y | y <- listPixels, (id_c (centroid x)) == (fst (getClosest y (clusters oldgen)))]) (clusters dupCentroid))
   where dupCentroid = (dupCentroidList oldgen)
+
+genNewCentroid :: [Pixel] -> Centroid -> Centroid
+genNewCentroid [] old = old
+genNewCentroid pixels old = Centroid (id_c old) (Pos3 r g b)
+  where r = (sum (map(\(Pixel (Pos2 _ _) (Pos3 r _ _)) -> r) pixels)) / (fromIntegral len)
+        g = (sum (map(\(Pixel (Pos2 _ _) (Pos3 _ g _)) -> g) pixels)) / (fromIntegral len)
+        b = (sum (map(\(Pixel (Pos2 _ _) (Pos3 _ _ b)) -> b) pixels)) / (fromIntegral len)
+        len = (length pixels)
+
+computeCentroids :: Generation -> Int -> Generation
+computeCentroids newgen nbCentroid = Generation (map(\x -> Cluster ((genNewCentroid (pointslist x) (centroid x))) (pointslist x)) (clusters newgen))
 
 firstGeneration :: Int -> [Pixel] -> Generation
 firstGeneration nbCentroid listPixels = Generation [(Cluster genRandCentroid []) | x <- [1..nbCentroid]]
